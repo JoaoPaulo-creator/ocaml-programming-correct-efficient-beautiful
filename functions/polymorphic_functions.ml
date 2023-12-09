a8e217865e2859a38c6331367d904fd3f7824ce9
JoaoPaulo-creator <joaosarto@gmail.com>
2023-12-08 22:31:17 -0300

    feat: polymorphic functions

---
+++ functions/polymorphic_functions.ml
@@ -0,0 +1,7 @@
+# let d a = a;;
+val d : 'a -> 'a = <fun>
+(* 'a indica um generic. Em ocaml, essas letras que representam qualquer coisa que recebederao um tipo, sao proununciados
+   conforme no alfabeto grego, ou seja, 'a -> alpha, 'b -> beta, etc.
+
+   Esses generics podem ter nomes mais descritivos, mas pro padrao ocaml retorna 'a, 'b, etc.
+ *)
