0b338a41ab5300df6444659183c8ef8423340be9
JoaoPaulo-creator <joaosarto@gmail.com>
2023-12-18 21:41:57 -0300

    feat: exceptions and exception handling

--- exceptions/exception_handling.ml
+++ exceptions/exception_handling.ml
@@ -1,4 +1,5 @@
 let safe_div x y =
-  (* semelhante ao pattern matching, mas nesse caso para lidar uma exception *)
+  (* semelhante ao pattern matching, mas nesse caso para lidar uma exception 
+     O primeiro padrao verifica se a divisao eh por zero, se sim, retorna 0 e assim nao mata o programa.*)
   try x / y with
   | Division_by_zero -> 0
